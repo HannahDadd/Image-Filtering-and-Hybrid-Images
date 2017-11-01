a3bd51a641faccaf4660024d7809e0f7bb469220
Hannah Dadd <hannahbdadd@googlemail.com>
2017-11-01 10:53:31 +0000

    Template Convolution algorithm added

--- src/main/java/uk/ac/soton/ecs/hbd1g15/MyConvolution.java
+++ src/main/java/uk/ac/soton/ecs/hbd1g15/MyConvolution.java
@@ -33,7 +33,26 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
 		}
 		FImage temporaryBufferImage = new FImage(temporaryImage);
 		
-		//
+		// Get the middle of the template distance
+		int middleColIndex = Math.floorDiv(this.kernel[1].length, 2);
+		int middleRowIndex = Math.floorDiv(this.kernel[0].length, 2);
+		
+		// Loop through each pixel in the image ignoring the border
+		for(int y=middleColIndex+1; y<image.getHeight()-middleColIndex; y++) {
+			for(int x=middleRowIndex+1; x<image.getWidth()-middleRowIndex; x++) {
+				float sum = 0;
+				
+				// Loop through each coefficient in the template
+				for(int templateX=0; templateX<this.kernel[0].length; templateX++) {
+					for(int templateY=0; templateY< this.kernel[1].length; templateY++) {
+						// Times each pixel value by the template value and add them all together
+						sum = sum + image.getPixelNative(x-middleRowIndex+templateX, y-middleColIndex+templateY) * this.kernel[templateX][templateY];
+					}
+				}
+				temporaryBufferImage.setPixelNative(x, y, sum);
+			}
+		}
+		
 		// hint: use FImage#internalAssign(FImage) to set the contents
 		// of your temporary buffer image to the image.
 	}
