dd3510c6ba1b5316842acbdd23858d887824b0a1
Hannah Dadd <hannahbdadd@googlemail.com>
2017-11-02 11:11:32 +0000

    Fixed size bug in convolution

--- src/main/java/uk/ac/soton/ecs/hbd1g15/MyConvolution.java
+++ src/main/java/uk/ac/soton/ecs/hbd1g15/MyConvolution.java
@@ -25,21 +25,21 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
 		}
 		
 		// Create a temporary, completely black image
-		float[][] temporaryImage = new float[image.width][image.height];
+		float[][] temporaryImage = new float[image.getHeight()][image.getWidth()];
 		for (int y=0; y<image.getHeight(); y++) {
 		    for (int x=0; x<image.getWidth(); x++) {
-		    	temporaryImage[x][y] = 0;
+		    	temporaryImage[y][x] = 0;
 		    }
 		}
 		FImage temporaryBufferImage = new FImage(temporaryImage);
 		
 		// Get the middle of the template distance
-		int middleColIndex = Math.floorDiv(this.kernel[1].length, 2);
 		int middleRowIndex = Math.floorDiv(this.kernel[0].length, 2);
+		int middleColIndex = Math.floorDiv(this.kernel[1].length, 2);
 		
 		// Loop through each pixel in the image ignoring the border
-		for(int y=middleColIndex+1; y<image.getHeight()-middleColIndex; y++) {
-			for(int x=middleRowIndex+1; x<image.getWidth()-middleRowIndex; x++) {
+		for(int x=middleRowIndex+1; x<image.getWidth()-middleRowIndex; x++) {
+			for(int y=middleColIndex+1; y<image.getHeight()-middleColIndex; y++) {
 				float sum = 0;
 				
 				// Loop through each coefficient in the template
@@ -55,5 +55,6 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
 		
 		// hint: use FImage#internalAssign(FImage) to set the contents
 		// of your temporary buffer image to the image.
+		image.internalAssign(temporaryBufferImage);
 	}
 }
